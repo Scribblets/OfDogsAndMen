783c6fa80b617be4ccf4803d1b03040d59c827f1
Lindsay <lrroberts0122@gmail.com>
2015-04-09 13:16:51 -0400

    GH-Pages
    Initial Commit

---
+++ js/video.js
@@ -0,0 +1,12 @@
+(function($) {
+	var playButton = $('#video-trigger');
+	playButton.click(function(e) {
+		e.preventDefault();
+		// html insert iframe with autoplay
+		$('.js-video').html('<iframe id="trailer" width="560" height="315" src="https://player.vimeo.com/video/116026152?autoplay=true" frameborder="0" allowfullscreen></iframe>');
+		
+		setTimeout(function() {
+			$('#trailer-overlay').fadeOut("slow");
+		}, 500);
+	});
+})(jQuery);
