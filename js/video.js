536cd93379d474898d34210097169b9a3c9acf7d
Lindsay <lrroberts0122@gmail.com>
2015-04-13 21:12:40 -0400

    Cleaned Up

--- js/video.js
+++ js/video.js
@@ -2,17 +2,4 @@
 	$(document).ready(function() {
 		$("#trailer-container").fitVids();
 	});
-	
-	/*
-var playButton = $('#video-trigger');
-	playButton.click(function(e) {
-		e.preventDefault();
-		// html insert iframe with autoplay
-		$('.js-video').html('<iframe id="trailer" width="560" height="315" src="https://player.vimeo.com/video/116026152" frameborder="0" allowfullscreen></iframe>');
-		
-		setTimeout(function() {
-			$('#trailer-overlay').fadeOut("slow");
-		}, 500);
-	});
-*/
 })(jQuery);
